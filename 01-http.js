51c30cd279ccf5af5c23eed5058648c49eca5877
Mas Fahru <imam@fahru.my.id>
2022-01-25 15:23:49 +0700

    using 2 spaces for tab

--- 01-http.js
+++ 01-http.js
@@ -1,37 +1,37 @@
 /**
  *  Core Module: HTTP
- *  
+ *
  *  Kita belajar membuat server sederhana dengan menggunakan module http
  */
 
 // Import module http dengan require
-const http = require('http')
+const http = require("http");
 
 // buat server dengan method createServer
 const server = http.createServer((req, res) => {
-    // Inisiasi variabel yang akan dipakai
-    let data
+  // Inisiasi variabel yang akan dipakai
+  let data;
 
-    // tampilkan pada console.log request yang diterima oleh server
-    console.log(req)
+  // tampilkan pada console.log request yang diterima oleh server
+  console.log(req);
 
-    /**
-     *  object req terdiri dari banyak data, namun yang paling sering dipakai ada 3
-     *  Yaitu url, method, headers
-     */
-    // buat object data berisi url, method, dan header
-    data = {
-        url: req.url,
-        method: req.method,
-        header: req.headers
-    }
+  /**
+   *  object req terdiri dari banyak data, namun yang paling sering dipakai ada 3
+   *  Yaitu url, method, headers
+   */
+  // buat object data berisi url, method, dan header
+  data = {
+    url: req.url,
+    method: req.method,
+    header: req.headers,
+  };
 
-    // Set response header berupa json
-    res.setHeader('Content-Type', 'application/json')
+  // Set response header berupa json
+  res.setHeader("Content-Type", "application/json");
 
-    // Kirim data berupa Json
-    res.end(JSON.stringify(data))
-})
+  // Kirim data berupa Json
+  res.end(JSON.stringify(data));
+});
 
 // set port server
-server.listen(3000)
+server.listen(3000);
--- 02-header.js
+++ 02-header.js
@@ -1,76 +1,83 @@
 /**
  *  Header HTTP
- *  
+ *
  *  Kita belajar menerima otentikasi berupa username dan password
  *  Juga belajar untuk mengirim token bearer
+ *
+ *  Ini adalah materi dasar agar kita tahu sedikit mengenai sistem di framework Backend seperti Expressjs, Hapijs, Nestjs, dll
  */
 
 // Import module http dengan require
-const http = require('http')
+const http = require("http");
 
 // buat server dengan method createServer
 const server = http.createServer((req, res) => {
-    /**
-     * Inisiasi variabel yang akan dipakai
-     * 
-     * dataHeader akan berisi object dari header request
-     * dataAuthorization akan berisi property authorization dari object dataHeader
-     * splitData akan berisi hasil pemisahan value authorization
-     * dataUser akan berisi kode base64 dari user dan password
-     * userPass akan berisi hasil decode base64 to string dari datauser
-     * dataResponse akan berisi data yang akan dikirim ke user
-     *  
-     * */
-    let dataHeader, dataAuthorization, splitData, dataUser, userPass, dataResponse
-    
-    // Set response header berupa json
-    res.setHeader('Content-Type', 'application/json')
+  /**
+   * Inisiasi variabel yang akan dipakai
+   *
+   * dataHeader akan berisi object dari header request
+   * dataAuthorization akan berisi property authorization dari object dataHeader
+   * splitData akan berisi hasil pemisahan value authorization
+   * dataUser akan berisi kode base64 dari user dan password
+   * userPass akan berisi hasil decode base64 to string dari datauser
+   * dataResponse akan berisi data yang akan dikirim ke user
+   *
+   * */
+  let dataHeader,
+    dataAuthorization,
+    splitData,
+    dataUser,
+    userPass,
+    dataResponse;
 
-    // Dapatkan header dari request
-    dataHeader = req.headers
+  // Set response header berupa json
+  res.setHeader("Content-Type", "application/json");
 
-    // tampilkan object dataHeader pada console.log
-    console.log(dataHeader)
+  // Dapatkan header dari request
+  dataHeader = req.headers;
 
-    // Dapatkan data otorisasi
-    dataAuthorization = dataHeader.authorization
+  // tampilkan object dataHeader pada console.log
+  console.log(dataHeader);
 
-    // jika tidak ada authorization, kita kirim data peringatan 
-    if (!dataAuthorization) {
-        dataResponse = {
-            data: 'Undefined Authorization'
-        }
-        // kirim ke client
-        res.end(JSON.stringify(dataResponse))
+  // Dapatkan data otorisasi
+  dataAuthorization = dataHeader.authorization;
 
-        // return agar behenti menjalankan kode setelah ini
-        return
-    }
-    
-    /**
-     *  Format data authorization adalah "{jenis} {Kode}"
-     *  contoh dalam kasus ini adalah "Basic dXNlcjEyMzpwYXNzMTIz" yang dipisahkan oleh spasi
-     *  kita akan ambil bagian "dXNlcjEyMzpwYXNzMTIz"
-     * */
-
-    // split string menjadi array dengan batasan spasi
-    splitData = dataAuthorization.split(" ")
-
-    // Dapatkan kode base64nya, ada di index 1
-    dataUser = splitData[1]
-    
-    // convert dataUser dari base64 ke string
-    userPass = Buffer.from(dataUser, 'base64').toString('utf-8')
-
-    // buat data respons berisi token dan userPass
+  // jika tidak ada authorization, kita kirim data peringatan
+  if (!dataAuthorization) {
     dataResponse = {
-        token: dataHeader.authorization,
-        userPass
-    }
+      data: "Undefined Authorization",
+    };
+    // kirim ke client
+    res.end(JSON.stringify(dataResponse));
 
-    // Kirim data berupa Json
-    res.end(JSON.stringify(dataResponse))
-})
+    // return agar behenti menjalankan kode setelah ini
+    return;
+  }
+
+  /**
+   *  Format data authorization adalah "{jenis} {Kode}"
+   *  contoh dalam kasus ini adalah "Basic dXNlcjEyMzpwYXNzMTIz" yang dipisahkan oleh spasi
+   *  kita akan ambil bagian "dXNlcjEyMzpwYXNzMTIz"
+   * */
+
+  // split string menjadi array dengan batasan spasi
+  splitData = dataAuthorization.split(" ");
+
+  // Dapatkan kode base64nya, ada di index 1
+  dataUser = splitData[1];
+
+  // convert dataUser dari base64 ke string
+  userPass = Buffer.from(dataUser, "base64").toString("utf-8");
+
+  // buat data respons berisi token dan userPass
+  dataResponse = {
+    token: dataHeader.authorization,
+    userPass,
+  };
+
+  // Kirim data berupa Json
+  res.end(JSON.stringify(dataResponse));
+});
 
 // set port server
-server.listen(3000)
+server.listen(3000);
